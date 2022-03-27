9501824d016ee59afd3df2c02e36064a46636350
hugosenari <hugosenari@gmail.com>
2022-03-27 18:15:35 -0300

    feat(nimpkgs): remove lock

--- main/flake.nix
+++ main/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-exprgrad-main.ref   = "refs/heads/main";
   inputs.src-exprgrad-main.owner = "can-lehmann";
   inputs.src-exprgrad-main.repo  = "exprgrad";
+  inputs.src-exprgrad-main.dir   = "";
   inputs.src-exprgrad-main.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
